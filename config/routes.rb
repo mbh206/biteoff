110a7cbb061eac2a1f7cf55ef23190ec41f5b370
Chafique ABIBOURAGUIMANE <shafiq.82@outlook.com>
2024-11-26 15:32:19 +0900

    Offer show draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "special_offers#index"
-  resources :offers, only: [:show, :new, :create, :update] do
+  resources :special_offers, only: [:show, :new, :create, :update] do
     resources :reviews, only: [:new, :create]
   end
-  resources :offers, only: [:destroy]
+  resources :special_offers, only: [:destroy]
   resources :reviews, only: [:update, :destroy]
 end
