a59579f04ae297c7f74986731b2319eaa7dcc706
christian velez <Csanjo@users.noreply.github.com>
2024-11-28 11:33:56 +0900

    added reviews tab

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
 
-  validates :description, presence: true, length: { in: 50..500 }
+  validates :description, presence: true, length: { in: 10..500 }
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
