39bb0bf90287a63999c0c95bc2bea68badd8d01d
Mark H <75957685+mbh206@users.noreply.github.com>
2024-11-28 18:20:55 +0900

    Update api_test.rb

--- db/api_test.rb
+++ db/api_test.rb
@@ -7,7 +7,7 @@ require 'json'
     "X-Goog-FieldMask" => "places.displayName"
 }.to_json
 
-uri = URI("https://places.googleapis.com/v1/places:searchText?key=AIzaSyD-gMyoO2yHKRe-_U16jooIRzyqOwF6iSk")
+uri = URI("https://places.googleapis.com/v1/places:searchText?key=")
 https = Net::HTTP.new(uri.host, uri.port)
 https.use_ssl = true
 req = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
