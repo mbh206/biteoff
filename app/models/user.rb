5d98cef5c5a5786924d5a063eae51fa4d093bb1f
Chafique ABIBOURAGUIMANE <shafiq.82@outlook.com>
2024-12-02 00:46:07 +0900

    Invitation model for friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :group_members, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  #added for invitations & friendship
+  has_many :invitations
+  has_many :pending_invitations, -> { where confirmed: false}, class_name: 'Invitation', foreign_key: "friend_id"
+
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true, length: { in: 5..25 }
   # Include default devise modules. Others available are:
@@ -12,4 +16,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :timeoutable
+
+  def friends
+    friends_i_sent_invitation = Invitation.where(user_id: id, confirmed: true).pluck(:friend_id)
+    friends_i_got_invitation = Invitation.where(friend_id: id, confirmed: true).pluck(:user_id)
+    ids = friends_i_sent_invitation + friends_i_got_invitation
+    User.where(id: ids)
+  end
+
+  def friend_with?(user)
+    Invitation.confirmed_record?(id, user.id)
+  end
+
+  def send_invitation(user)
+    invitations.create(friend_id: user.id)
+  end
+
+  def accept_invitation(user)
+    if !pending_invitations.empty?
+      pending_invitations.where(user_id: user.id).confirmed
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -156,9 +156,6 @@ shinjuku_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query
 meguro_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Meguro&fields=name,formatted_address,photo,rating,opening_hours&key=#{ENV['GOOGLE_MAPS_KEY']}"
 ginza_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Ginza&fields=name,formatted_address,photo,rating,opening_hours&key=#{ENV['GOOGLE_MAPS_KEY']}"
 
-
-
-
 puts "Creating restaurants in Shibuya"
 
 # Restaurant seeds with names, categories, and coordinates (latitude and longitude)
@@ -298,46 +295,14 @@ end
 
 puts "....created #{Restaurant.count} restaurants!"
 
-# # user_serialized = URI.parse(ginza_url).read
-# # ginza_json = JSON.parse(user_serialized)
-
-# # ginza_restaurants = ginza_json["results"]
-# # ginza_restaurants.each do |restaurant|
-# #   puts restaurant["name"]
-#   # puts restaurant["geometry"]["location"]["lat"]
-#   # puts restaurant["geometry"]["location"]["lng"]
-# end
-
-
-# CATEGORIES = ["burger", "ramen", "sushi", "desserts", "healthy", "kebabs", "pizza", "tacos", "sandwiches"]
-
-# puts "Creating 10 Restaurants..."
-
-# restaurant_photo = URI.parse("https://upload.wikimedia.org/wikipedia/commons/b/bb/ELSASS_-_Munchhausen_-_RESTAURANT_a_la_ROSE_-_panoramio.jpg").open
-
-# 10.times do
-#   restaurant_name = Faker::Restaurant.unique.name
-#   restaurant = Restaurant.new(
-#     name: restaurant_name,
-#     address: "日本, 〒153-0063 東京都目黒区 目黒#{rand(1..3)}丁目#{rand(1..10)}番#{rand(1..3)}号",
-#     category: CATEGORIES.sample
-#   )
-
-#    restaurant.user = owner
-#    restaurant.photos.attach(io: restaurant_photo, filename: "restaurant.jpg", content_type: "image/jpg")
-#    restaurant.save!
-# end
-# puts '... created 10 restaurants'
-
-
 deals = ["Happy hour Deals", "Early Bird Special", "Buy one get one", "20% off", "$10 off"]
+
 puts "Creating 10 Special Offers"
 description = ["Great savings", "Unbeatable Prices", "Amazing Offer", "Amazing deal", "Unbeatable Price"]
 
 def random_date(start_date, end_date)
   rand(start_date..end_date)
 end
-
 
 puts 'Creating offers'
 
@@ -415,3 +380,4 @@ OffersCollection.create!(special_offer: SpecialOffer.all[1], special_offers_list
 chafsparty = SpecialOffersList.create!(name: "Chaf s Party")
 OffersCollection.create!(special_offer: SpecialOffer.all[2], special_offers_list: chafsparty)
 OffersCollection.create!(special_offer: SpecialOffer.all[3], special_offers_list: chafsparty)
+
