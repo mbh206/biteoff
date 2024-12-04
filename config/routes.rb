bb09cc06995166bbb7f9308d9e62fb9d6333ce8d
christian velez <Csanjo@users.noreply.github.com>
2024-12-04 11:42:10 +0900

    fixed alerts and some stlye

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :voting_sessions, only: [:show, :create, :update] do
     resources :votes, only: [:update,:create]
   end
-  get "/createsessioninbackground/:special_offer_id", to: "voting_sessions#csib", as: "csib"
+  post "/createsessioninbackground/:special_offer_id", to: "voting_sessions#csib", as: "csib"
   resources :voters, only: [:destroy]
   # Steve's add
   resources :locations, only: [:index]
