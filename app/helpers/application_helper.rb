4eaa69de01c6e2de2fd21617ddf02b65344a880b
Chafique ABIBOURAGUIMANE <shafiq.82@outlook.com>
2024-12-05 10:21:30 +0900

    last push

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,9 @@ module ApplicationHelper
     #@current_user_voting_session ||= VotingSession.find_by(user_id: current_user.id)
     session = current_voting_session(current_user)
     vote = Vote.where(voting_session_id: session).group(:special_offer_id).order('COUNT(special_offer_id) DESC').limit(1).count
-    sp_offer = SpecialOffer.find(vote.keys.first)
-    @current_user_voted_location = sp_offer.restaurant
-    
+    @sp_offer = SpecialOffer.find(vote.keys.first)
+    @current_user_voted_location = @sp_offer.restaurant
+    return [@sp_offer, @current_user_voted_location]
   end
 
 end
